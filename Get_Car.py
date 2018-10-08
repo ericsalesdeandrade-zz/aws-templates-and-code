f37b2a79b3a4353ac236a701020f781d4c97655e
ericsda <Eric.SalesDeAndrade@collinsongroup.com>
2018-10-08 19:31:43 +0100

    Adding Bucket Policy and updated Lambda function

--- Get_Car.py
+++ Get_Car.py
@@ -6,10 +6,13 @@ def get_car(event, context):
         client_s3 = boto3.client('s3')
         brand_upper = event["brand"].upper()
         bucket = s3.Bucket("car-images-hd")
+
         for obj in bucket.objects.filter(Prefix=brand_upper + '/'):
             url = "https://s3-eu-west-1.amazonaws.com/" + obj.bucket_name + "/" + obj.key
-            return url
+            return ("Image URL is {0}".format(url))
+        return("*** Failure to retrieve Car Image - Please check your request ***")
+
     except BaseException as error:
         print("*** Failure to retrieve Car Image - Please check your request ***")
-        print(error)
-        return "*** Failure to retrieve Car Image - Please check your request ***"
+        return str(error)
+    
